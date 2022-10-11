0f7bc0dc1cff029b292dc90e4e1a69c83a640f3f
Gogyungho <whohow10@gmail.com>
2022-10-11 21:00:49 +0900

    next setting

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ko">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
+
+// 서버에서만 랜더링되고,
+// 이벤트 랜들러가 작동하지 않고, css도 사용하지 않는다.
