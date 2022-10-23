e57d2e1f13138162e7e9d7398a5cd75e971516a3
Gogyungho <whohow10@gmail.com>
2022-10-23 23:05:18 +0900

    로더 추가

--- src/component/Loader/Loader.js
+++ src/component/Loader/Loader.js
@@ -5,17 +5,19 @@ const Loader = () => {
   return (
     <div className={styles.loader}>
       <div className={styles.loader_dots}>
-        <div className={styles.loader_dots}>
-          <i className={(styles.fa - solid, styles.fa - circle)}></i>
+        <div className={styles.loader_dot}>
+          <i className={(styles.fa_solid, styles.fa_circle)}></i>
         </div>
-        <div className={styles.loader_dots}>
-          <i className={(styles.fa - solid, styles.fa - circle)}></i>
+        <div className={styles.loader_dot}>
+          <i className={(styles.fa_solid, styles.fa_circle)}></i>
         </div>
-        <div className={styles.loader_dots}>
-          <i className={(styles.fa - solid, styles.fa - circle)}></i>
+        <div className={styles.loader_dot}>
+          <i className={(styles.fa_solid, styles.fa_circle)}></i>
         </div>
       </div>
       <span className={styles.loader_text}>Loading</span>
     </div>
   );
 };
+
+export default Loader;
